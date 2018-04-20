653bff20cc4c76f7dc7f1c8c2cd6b8159ea6337e
Jan Peter Mamuric <webdev.jmamuric@gmail.com>
2018-04-20 14:20:12 -0700

    added sidenav , started main component

--- src/react/components/Home.js
+++ src/react/components/Home.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Home = () => {
-  return (
-    <main className="hotel-view">
-      Home
-    </main>
-  )
+import Icon from './Icon';
+
+
+const images = [
+  { name : 'hotel-1'},
+  { name : 'hotel-2'},
+  { name : 'hotel-3'},
+]
+
+class Home extends Component {
+  render() {
+    return (
+      <main className="hotel-view">
+        <div className="gallery">
+          {
+            images.map( img => (
+              <figure key={img.name} className="gallery__item">
+                <img src={require(`../../assets/img/${img.name}.jpg`)} alt="" className="gallery__img"/>
+              </figure>
+            ))
+          }
+        </div>
+
+        <div className="overview">
+          <h1 className="overview__heading">
+            Cosmopolitan
+          </h1>
+
+          <div className="overview__stars">
+            {/* generate dynamic ratings*/}
+
+            <Icon name='star' />
+            <Icon name='star' />
+            <Icon name='star' />
+            <Icon name='star' />
+          </div>
+
+          <div className="overview__location">
+            <Icon name='location-pin'/>
+            <button className='btn-inline'>Las Vegas, Nevada</button>
+          </div>
+
+          <div className='overview__rating'>
+            <div className='overview__rating-average'>8.6</div>
+            <div className="overview__rating-count">421</div>
+          </div>
+
+        </div>
+      </main>
+    )
+  }
 }
 
 export default Home;
--- src/react/components/Sidebar.js
+++ src/react/components/Sidebar.js
@@ -1,33 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Icon from './Icon';
 
 const SIDE_NAV_ICON = [
-  { name: 'home', title: 'Hotel' },
-  { name: 'aircraft', title: 'Flight' },
-  { name: 'key', title: 'Car' },
-  { name: 'map', title: 'Attractions' }
+  { name: 'home', title: 'Hotel', active: true, activeClass: 'sidebar-nav__item--active'},
+  { name: 'aircraft', title: 'Flight', activeClass: 'sidebar-nav__item--active'},
+  { name: 'key', title: 'Car', activeClass: 'sidebar-nav__item--active' },
+  { name: 'map', title: 'Attractions', activeClass: 'sidebar-nav__item--active' }
 ]
 
-const Sidebar = () => {
-  return (
-    <nav className="sidebar">
-      <ul className="sidebar-nav">
-        {
-          SIDE_NAV_ICON.map( (icon,i) => (
-            <li key={i} className="sidebar-nav__item">
-              <a href="" className="sidebar-nav__link">
-                <Icon name={icon.name} />
-                <span>{icon.title}</span>
-              </a>
-            </li>
-          ))
-        }
-      </ul>
+class Sidebar extends Component {
+  state = { isActive: false };
 
-      <div className="legal"> &copy; Jan Peter Mamuric 2018. All rights reserved.</div>
-    </nav>
-  )
+  handleOnClick(name) {
+    console.log(name);
+  }
+
+  render() {
+    return (
+      <nav className="sidebar">
+        <ul className="sidebar-nav">
+          {
+            SIDE_NAV_ICON.map( (icon,i) => (
+              <li
+                key={i}
+                className={ icon.active ? `sidebar-nav__item ${icon.activeClass}` : 'sidebar-nav__item '}
+                onClick={(name)=>this.handleOnClick(icon.name)}>
+                <a href="" className="sidebar-nav__link">
+                  <Icon name={icon.name} />
+                  <span>{icon.title}</span>
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+
+        <div className="legal"> &copy; Jan Peter Mamuric 2018. All rights reserved.</div>
+      </nav>
+    )
+  }
 }
 
 export default Sidebar;
